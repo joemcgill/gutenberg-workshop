aa1ce8e8021fa2d6d9c64eb3f6fb8091870ad989
Joe McGill <joemcgill@gmail.com>
2018-07-12 11:24:06 -0500

    Fix typo

--- src/block/block.js
+++ src/block/block.js
@@ -13,7 +13,7 @@ const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 
 /**
- * Register: aa Gutenberg Block.
+ * Register: a Gutenberg Block.
  *
  * Registers a new block provided a unique name and an object defining its
  * behavior. Once registered, the block is made editor as an option to any
